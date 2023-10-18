ee7e99938791ee9d39c34c2b9111abd8ca53b5cf
Philipp Wegner <philipp.wegner@scai.fraunhofer.de>
2023-10-18 20:50:06 +0200

    added code

--- frontend/variable-mapper/src/views/NewsView.vue
+++ frontend/variable-mapper/src/views/NewsView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-center align-middle h-full">
-    <p class="text-2xl">
+  <div class="flex justify-center px-16 py-16 align-middle">
+    <p class="text-2xl bg-gray-100 rounded py-6 px-6">
         We're thrilled to share that our team is actively working on PD-Mapper, a specialized tool dedicated to the semantic harmonization of variable conventions within Parkinson's disease cohorts. This tool aims to simplify the complexities of data integration and enhance collaboration among researchers in the Parkinson's disease community. Stay tuned for more updates on PD-Mapper and its potential to revolutionize data management in PD research.
     </p>
   </div>
